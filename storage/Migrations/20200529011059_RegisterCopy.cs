b35329cb2ce92df3f259e0ca2d1ee1fe7126f805
Terry Burns-Dyson <terrybd@gmail.com>
2020-05-28 21:18:34 -0400

    Added migration for Copies

---
+++ storage/Migrations/20200529011059_RegisterCopy.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GameTrove.Storage.Migrations
+{
+    public partial class RegisterCopy : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Copies",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    GameId = table.Column<Guid>(nullable: false),
+                    Tags = table.Column<string>(nullable: true),
+                    Cost = table.Column<decimal>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Copies", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Copies");
+        }
+    }
+}
