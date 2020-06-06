cbd6a30707808e1e5110f7ee414e65fdf0c9669f
Terry Burns-Dyson <terrybd@gmail.com>
2020-06-06 10:31:39 -0400

    Migrations for Genres

--- storage/Migrations/20200606143050_AddGenres.Designer.cs
+++ storage/Migrations/20200606143050_AddGenres.Designer.cs
@@ -4,14 +4,16 @@ using GameTrove.Storage;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace GameTrove.Storage.Migrations
 {
     [DbContext(typeof(GameTrackerContext))]
-    partial class GameTrackerContextModelSnapshot : ModelSnapshot
+    [Migration("20200606143050_AddGenres")]
+    partial class AddGenres
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ storage/Migrations/20200606143050_AddGenres.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GameTrove.Storage.Migrations
+{
+    public partial class AddGenres : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Genres",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Genres", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TitleGenres",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    TitleId = table.Column<Guid>(nullable: false),
+                    GenreId = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TitleGenres", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Genres");
+
+            migrationBuilder.DropTable(
+                name: "TitleGenres");
+        }
+    }
+}
