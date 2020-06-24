64ba86fa4f1fcd6693a561fcd6e7ba47a250260a
Terry Burns-Dyson <terrybd@gmail.com>
2020-06-23 22:45:41 -0400

    Registering user
    Create handler to register or return existing user

---
+++ application/Commands/Handlers/RegisterUserHandler.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Threading;
+using System.Threading.Tasks;
+using GameTrove.Storage;
+using GameTrove.Storage.Models;
+using MediatR;
+
+namespace GameTrove.Application.Commands.Handlers
+{
+    public class RegisterUserHandler : IRequestHandler<RegisterUser, Guid>
+    {
+        private readonly GameTrackerContext _context;
+
+        public RegisterUserHandler(GameTrackerContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Guid> Handle(RegisterUser request, CancellationToken cancellationToken)
+        {
+            var existing = _context.Users.SingleOrDefault(u => u.Email == request.Email);
+
+            if (existing != null)
+            {
+                return existing.Id;
+            }
+
+            var user = new User
+            {
+                Email = request.Email,
+                Identifier = request.Identifier
+            };
+
+            _context.Users.Add(user);
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return user.Id;
+        }
+    }
+}
--- storage/GameTrackerContext.cs
+++ storage/GameTrackerContext.cs
@@ -6,13 +6,13 @@ namespace GameTrove.Storage
     public class GameTrackerContext : DbContext
     {
         public DbSet<Copy> Copies { get; set; }
-        public DbSet<Genre> Genres { get; set; }
-        public DbSet<Title> Titles { get; set; }
-        public DbSet<TitleGenre> TitleGenres { get; set; }
         public DbSet<Game> Games { get; set; }
-
+        public DbSet<Genre> Genres { get; set; }
         public DbSet<Platform> Platforms { get; set; }
         public DbSet<GameImage> PlatformGameImages { get; set; }
+        public DbSet<Title> Titles { get; set; }
+        public DbSet<TitleGenre> TitleGenres { get; set; }
+        public DbSet<User> Users { get; set; }
 
         public GameTrackerContext(DbContextOptions<GameTrackerContext> options) : base(options)
         {
