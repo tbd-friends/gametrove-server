1c5636cd9e19ff8f9e0f24cb5b1560734c53c38d
Terry Burns-Dyson <terrybd@gmail.com>
2020-07-04 13:41:02 -0400

    Token Generation Structure
    Create TenantInvites to store current generated invite code and expiration

---
+++ storage/Models/TenantInvite.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GameTrove.Storage.Models
+{
+    public class TenantInvite
+    {
+        public Guid Id { get; set; }
+        public Guid TenantId { get; set; }
+        public string Token { get; set; }
+        public DateTime Expiration { get; set; }
+    }
+}
