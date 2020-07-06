ea616da13e08ed164d9b83a6dd7183a2aa56b1d3
Terry Burns-Dyson <terrybd@gmail.com>
2020-07-06 14:24:52 -0400

    Removed Identifier
    No longer require tracking something "unique" against the user,

---
+++ storage/Migrations/20200706182401_RemoveIdentifier.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GameTrove.Storage.Migrations
+{
+    public partial class RemoveIdentifier : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Identifier",
+                table: "Users");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Identifier",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
